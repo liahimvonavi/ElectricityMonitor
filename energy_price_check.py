1597baa8a584346668f5dcdbb8811c515952c511
Mihail <mihail.1994.ivanov@gmail.com>
2025-06-03 17:18:59 +0300

    headless browser and mask wait

--- energy_price_check.py
+++ energy_price_check.py
@@ -10,7 +10,7 @@ load_dotenv()
 
 threshold = 20
 chrome_options = webdriver.ChromeOptions()
-chrome_options.add_argument('--headless')  # <--- add this line!
+chrome_options.add_argument('--headless')
 chrome_options.add_argument('--window-size=1920,1080')
 driver = webdriver.Chrome(options=chrome_options)
 driver.get(os.getenv('PRICES_URL'))
